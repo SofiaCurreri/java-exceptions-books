28b22b97d648f370c171c3187342df957554473b
SofiaCurreri <sofiacurreri@gmail.com>
2023-06-07 14:55:24 +0200

    Book class + asking for book quantity in the catalog

---
+++ src/org/lessons/java/library/Book.java
@@ -0,0 +1,52 @@
+package org.lessons.java.library;
+
+public class Book {
+    //ATTRIBUTES
+    private String title;
+    private int numPages;
+    private String author;
+    private String editor;
+
+    //CONSTRUCTOR
+
+    public Book(String title, int numPages, String author, String editor) {
+        this.title = title;
+        this.numPages = numPages;
+        this.author = author;
+        this.editor = editor;
+    }
+
+    //GETTER
+    public String getTitle() {
+        return title;
+    }
+
+    public int getNumPages() {
+        return numPages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    //SETTER
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setNumPages(int numPages) {
+        this.numPages = numPages;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+}
---
+++ src/org/lessons/java/library/Catalog.java
@@ -0,0 +1,13 @@
+package org.lessons.java.library;
+import java.util.Scanner;
+public class Catalog {
+    public static void main(String[] args) {
+        Scanner book = new Scanner(System.in);
+
+        System.out.println("Quanti libri ci sono nel catalogo?");
+        int n = book.nextInt();
+
+        String[] catalog = new String[n];
+
+    }
+}
